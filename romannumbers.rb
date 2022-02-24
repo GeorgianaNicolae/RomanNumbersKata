fde1c1b9d867c048881b4e4e63d862e9a313533e
GeorgianaNicolae <georgiana.nicolae@madetech.com>
2022-02-24 20:46:31 +0000

    It check is the the arguments are integers

--- romannumbers.rb
+++ romannumbers.rb
@@ -1,3 +1,7 @@
 class RomanNumbers 
-    
+    #Given an integer input, produce output in Roman Numeral format.
+    def roman_numbers(int)
+        return int.is_a? Integer
+        
+    end
 end
--- spec/romannumbers_spec.rb
+++ spec/romannumbers_spec.rb
@@ -5,4 +5,21 @@ describe RomanNumbers do
         @romannumbers = described_class.new
     end
 
+    it "returns true if the argument in an integer" do
+
+        result = @romannumbers.roman_numbers(1)
+        expect(result).to eq(true)
+    end
+
+    it "returns false if the argument in not integer" do
+
+        result = @romannumbers.roman_numbers("1")
+        expect(result).to eq(false)
+    end
+
+    it "returns false if the argument in not integer" do
+
+        result = @romannumbers.roman_numbers("1")
+        expect(result).to eq(false)
+    end
 end
